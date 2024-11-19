39b0adf26008c9637100f8d79a0cfa0015d1150b
SebastianHernandez69 <sh3641046@gmail.com>
2024-11-18 18:37:11 -0600

    fix build error

--- src/components/sidebar/header.tsx
+++ src/components/sidebar/header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { use, useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
 
