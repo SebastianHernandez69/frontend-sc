cccf4107706d6c369d70e249a743f0b9dca35aeb
SuazoXD <suazocarlos50@gmail.com>
2024-11-29 21:51:54 -0600

    Historia de Eliminar Preguntas Listo

--- src/app/home/interfaces/question-interface.ts
+++ src/app/home/interfaces/question-interface.ts
@@ -30,3 +30,29 @@ export interface Question{
         }
     }[];
 }
+
+// interfaces/question-interface.ts
+export interface Question {
+    id: string;  // Asegúrate de que 'id' esté en la interfaz
+    titulo: string;
+    descripcion: string;
+    fechaPublicacion: string;
+    materia: {
+        materia: string;
+    };
+    imgpregunta: Array<{ img: string }>;
+}
+// interfaces/question-interface.ts
+
+export interface Question {
+    idPregunta: number;
+    titulo: string;
+    descripcion: string;
+    fechaPublicacion: string;
+    materia: {
+      materia: string;
+    };
+    imgpregunta: Array<{ img: string }>;
+    estado: "pendiente" | "aceptada" | "contestada";  // Agregar esta línea para que 'estado' sea una propiedad válida
+  }
+  
